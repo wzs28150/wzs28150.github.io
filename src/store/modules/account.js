5673fc38a1b9a1a58c8265e0b20d6ef1bb367e0d
wzs28150 <wz666s@163.com>
2018-07-04 11:41:15 +0800

    add

--- src/store/modules/account.js
+++ src/store/modules/account.js
@@ -1,5 +1,5 @@
 /**
- * Created by bingoogolapple on 2017/5/31.
+ * Created by wzs28150 on 2017/5/31.
  */
 import { makeAction } from '../util'
 
@@ -12,29 +12,30 @@ const state = {
   accessToken: localStorage.getItem(LS_KEY_ACCESS_TOKEN),  // 这个不要修改，这个不要修改，这个不要修改。当前登录用户的 GitHub AccessToken
   auth: {
     proxy: 'https://cors-anywhere.herokuapp.com/https://github.com/login/oauth/access_token', // 这个不要修改，这个不要修改，这个不要修改。
-    clientID: '8fe09ec96875938b908d',   // 改成你在 https://github.com/settings/applications/new 新建的 OAuth application 的 Client ID
-    clientSecret: '46df51ccde6f3499c3b90861bba660fb1bcf15e4'  // 改成你在 https://github.com/settings/applications/new 新建的 OAuth application 的 Client Secret
+    clientID: '404d7eadcfa667c1f691',   // 改成你在 https://github.com/settings/applications/new 新建的 OAuth application 的 Client ID
+    clientSecret: 'd04d3d63a432c4078a490af5631ee4f771e2a6fd'  // 改成你在 https://github.com/settings/applications/new 新建的 OAuth application 的 Client Secret
   },
   gitHubUser: null,  // 这个不要修改，这个不要修改，这个不要修改。
-  gitHubUsername: 'bingoogolapple',  // 修改成你自己的 GitHub 账号
-  copyright: '2014 - 2017',  // 修改成你自己的
-  recordNumber: '蜀ICP备17023604号',  // 修改成你自己的备案编号，如果没有备案的话就设置为 null
-  repo: 'bingoogolapple/bingoogolapple.github.io',  // 记录 Issue 的仓库的全名「用户名/仓库名」
+  gitHubUsername: 'wzs28150',  // 修改成你自己的 GitHub 账号
+  copyright: '2018 - 2019',  // 修改成你自己的
+  recordNumber: null,  // 修改成你自己的备案编号，如果没有备案的话就设置为 null
+  repo: 'wzs28150/meblog',  // 记录 Issue 的仓库的全名「用户名/仓库名」
   pageSize: 10,  // 博客列表每页显示多少条博客
   showQQGroup: true,  // 如果要显示你自己的 QQ 群二维码图片的话这里配置成 true 并且替换「BGAIssueBlog-Web/static/img/qq-group.png」为你自己的 QQ 群二维码图片，否则配置成 false 即可
   thirdPartySite: [  // 配置你想在左上角展示的第三方站点信息
     {
       img: 'static/img/github.png',  // 第三方站点图标，存放在「BGAIssueBlog-Web/static/img」目录中
-      url: 'https://github.com/bingoogolapple'  // 第三方站点的 url
+      url: 'https://github.com/wzs28150'  // 第三方站点的 url
     },
     {
       img: 'static/img/weibo.png',
-      url: 'http://weibo.com/bingoogol'
-    },
-    {
-      img: 'static/img/git.png',
-      url: 'https://bingoogolapple.gitbooks.io/bgalearningnotes-git/content'
+      url: 'http://weibo.com/1003418012'
     }
+    // ,
+    // {
+    //   img: 'static/img/git.png',
+    //   url: 'https://wzs28150.gitbooks.io/bgalearningnotes-git/content'
+    // }
     // 如果还有其他站点需要显示，继续在这里追加
   ]
 }
